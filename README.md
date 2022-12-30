2253fce083dc2be48fbf425e42af2060bcb6968e
Mona Lisa <naomiroznik@gmail.com>
2022-12-30 14:56:17 -0500

    edits, images, readme, etc. project is still looking very sad

---
+++ README.md
@@ -0,0 +1,42 @@
+# Server-Side APIs Challenge: Weather Dashboard
+
+## Description
+
+For this project, I attempted to build a a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+Using [OpenWeatherMap](https://openweathermap.org/forecast5) the base URL should look like the following: 
+```https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}```
+
+I really struggled with this one! And now I'm finding myself in a bit of a time crunch so I'm hoping to submit this project for partial credit. I had so much difficulty with the API. I will re-work this project so I can add it to my portfolio.
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Mockup
+
+Please view my very sad project below. I know it's incomplete but I am hoping to earn as much partial credit as possible.
+
+![Alt text](assets/images/mockup.png "sad")
+
+Below is the mockup from the module assignment:
+
+![Alt text](assets/images/mockup2.png "very sad")
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,54 @@
-// var APIkey = "5e0fc062533663404b1acd91a8cbb20b";
-// // // https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
+var APIkey = "5e0fc062533663404b1acd91a8cbb20b";
+// // https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
 
-// var cityInputEl = $('#city-input');
-// var searchBtn = $('#search-button');
-// var currentCity;
+var cityInputEl = $('#city-input');
+var searchBtn = $('#search-button');
+var currentCity;
 
-// function getWeather(data) {
+function getWeather(data) {
 
-//   var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`
-//   fetch(requestUrl)
-//       .then(function(response) {
-//           return response.json();
-//       })
-//     }
+  var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${APIkey}`
+  fetch(requestUrl)
+      .then(function(response) {
+          return response.json();
+      })
+    }
 
-// function getCoordinates() {
-//   // https://openweathermap.org/forecast5#name5
-//   var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=imperial&appid=${APIkey}`;
-//   var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
+function getCoordinates() {
+  // https://openweathermap.org/forecast5#name5
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=imperial&appid=${APIkey}`;
+  var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
 
-//   // https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=5e0fc062533663404b1acd91a8cbb20b
+  // https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=5e0fc062533663404b1acd91a8cbb20b
 
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       if (response.status >= 200 && response.status <= 299) {
-//         return response.json();
-//       } else {
-//         throw Error(response.statusText);
-//       }
-//     })
-//     .then(function (data) {
+  fetch(requestUrl)
+    .then(function (response) {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    })
+    .then(function (data) {
 
-//       var cityInfo = {
-//         city: currentCity,
-//         lon: data.coord.lon,
-//         lat: data.coord.lat
-//       }
+      var cityInfo = {
+        city: currentCity,
+        lon: data.coord.lon,
+        lat: data.coord.lat
+      }
 
-//       storedCities.push(cityInfo);
-//       localStorage.setItem("cities", JSON.stringify(storedCities));
+      storedCities.push(cityInfo);
+      localStorage.setItem("cities", JSON.stringify(storedCities));
 
-//       displaySearchHistory();
+      displaySearchHistory();
 
-//       return cityInfo;
-//     })
-//     .then(function (data) {
-//       getWeather(data);
-//     })
-//   return;
-// }
+      return cityInfo;
+    })
+    .then(function (data) {
+      getWeather(data);
+    })
+  return;
+}
 
 
 
